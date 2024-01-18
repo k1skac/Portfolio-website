c2ae6463d25d386a1f90af8d91b9df7417edd631
k1skac <110919738+k1skac@users.noreply.github.com>
2024-01-18 17:42:24 +0100

    education  component

---
+++ src/components/Education.js
@@ -0,0 +1,86 @@
+
+const Education = (styles) => {
+ return(
+    <section class="education" id="education">
+        <h2 class="heading">My <span>Journey</span>
+         <span class="animate scroll" style={styles.style[0]}></span> 
+        </h2>
+        <div class="education-row">
+            <div class="education-column">
+                <h3 class="title">Education
+                 <span class="animate scroll" sstyle={styles.style[1]}></span> 
+                </h3>
+                <div class="education-box">
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class="bx bxs-calendar"></i>2022-2023</div>
+                            <h3>GFA Backend Developer training</h3>
+                            <p>After feeling that teaching was no longer enough for me, I was a bit burnt out and wanted something completely new.
+                                 I had been interested in programming for a long time, but I was always afraid to take the plunge. 
+                                 I finally plucked up the courage to apply to GFA as a backend developer. 
+                                 I passed all my exams on the first attempt and fell more and more in love with programming. Now I want to use and expand the knowledge I have gained here.
+                            </p>
+                        </div>
+                    </div>
+                        <div class="education-content">
+                            <div class="content">
+                                <div class="year"><i class="bx bxs-calendar"></i>2002-2008</div>
+                                <h3>Master Degree -University of Pécs</h3>
+                                <p>In 2002 I was admitted to the Faculty of Humanities at the University of Pécs. 
+                                    Here I started my studies to become a teacher of Hungarian language and literature and history.
+                                     I wanted to work with children, to shape them, to educate them and to pass on my knowledge.
+                                      It is a great thing to shape the future generation!</p>
+                            </div>
+                        </div>
+                        <div class="education-content">
+                            <div class="content">
+                                <div class="year"><i class="bx bxs-calendar"></i>2002-2008</div>
+                                <h3>Master Degree -University of Pécs</h3>
+                                <p>During this time, I gained a double degree, becoming a teacher of Hungarian language and literature and a history teacher at the same time. At that time,
+                                     I thought that educating the future generation would be my path.</p>
+                            </div>
+                        </div>
+                         <span class="animate scroll" style={styles.style[2]}></span> 
+                    </div>
+                </div>
+          
+
+            <div class="education-column">
+                <h3 class="title">Experience
+                 <span class="animate scroll" style={styles.style[3]}></span> 
+                </h3>
+                <div class="education-box">
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class="bx bxs-calendar"></i>2022-2023</div>
+                            <h3>GFA Backend Developer </h3>
+                            <p>Now that I have completed the course, I would like to finally enter the job market as an aspiring backend developer or a beginner fullstack developer.
+                                 I want to start as a member of a good community, I hope you can help me.  </p>
+                        </div>
+                        </div>
+                        <div class="education-content">
+                            <div class="content">
+                                <div class="year"><i class="bx bxs-calendar"></i>2018-2023</div>
+                                <h3>PMG</h3>
+                                <p>After that I wanted a bigger challenge, so I joined the local Perczel Mór High School, also as a secondary school teacher and class teacher. Here I could expect more professionalism, more responsibility and more work. 
+                                    Every year I improved the Hungarian entrance exams of the students in the district and every year (often several times, in several classes)
+                                     I gave the students their school-leaving exams. I look back on these five years with fond memories. </p>
+                            </div>
+                        </div>
+                        <div class="education-content">
+                            <div class="content">
+                                <div class="year"><i class="bx bxs-calendar"></i>2008-2018</div>
+                                <h3>Krúdy Company</h3>
+                                <p>My first job was at the Gyula Krúdy Vocational School in Siofok, where I worked as a secondary school teacher and class teacher for 10 years. 
+                                    Here I learned the basics of teaching, the most important elements of my profession, and I spent these 10 years working hard and in a good atmosphere.</p>
+                            </div>
+                        </div>
+                        <span class="animate scroll" style={styles.style[4]}></span> 
+                    </div>
+                </div>
+            </div>
+            <script src=" my-portfolio\src\App.js"/>
+    </section>
+ )
+}
+export default Education;
