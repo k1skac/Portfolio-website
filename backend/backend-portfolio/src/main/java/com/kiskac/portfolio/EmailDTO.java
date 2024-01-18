7ced7966fb10ae28c29c30d032fe4c13aa73e741
k1skac <110919738+k1skac@users.noreply.github.com>
2024-01-18 17:58:40 +0100

    the emailDTO added to the backend project

---
+++ backend/backend-portfolio/src/main/java/com/kiskac/portfolio/EmailDTO.java
@@ -0,0 +1,11 @@
+package com.kiskac.portfolio;
+import lombok.Data;
+
+@Data
+public class EmailDTO {
+     String email;
+     String mobileNum;
+     String message;
+     String subject;
+     String fullName;
+}
